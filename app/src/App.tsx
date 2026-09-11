fe6405c216331b1f91c9fe4e72f88d0fcd1d7844
Asaaa666 <s2022026@asano.ed.jp>
2026-09-11 17:47:55 +0000

    aaa

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from 'react';//アプリを動かす使�
 import { normalizeCategoryValue, getUnifiedLocationGroup, matchesSearchQuery } from './searchUtils';
 
 // ⚠️ STEP 2で取得したGASのWebアプリURLをここに貼り付けてください
-const GAS_API_URL = "https://script.google.com/macros/s/AKfycbw33wQjhzaXRUMx4gI6qi1CrMaYwZB2YbR01Ti582EOhPu-mbh6jgdx1mhILboYs4CDHg/exec";//読み込むスプレットシートを選択するコード
+const GAS_API_URL = "https://docs.google.com/spreadsheets/d/1W4vJaSM0jlOQcjE-TstUby6v0K1bJeSY3GnSH8BDKhA/edit?usp=sharing";//読み込むスプレットシートを選択するコード
 
 interface Group {//団体の情報を格納するためのインターフェース
   name: string;//団体名
